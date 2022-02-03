81185089bf3000a55008242682c791029fe3c15e
Arash-HR <arashhosseinirezaei@gmail.com>
2022-02-03 11:33:06 +0330

    seperate home from app component and create task component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'tasks', component: TasksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
